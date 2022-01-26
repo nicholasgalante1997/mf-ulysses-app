02f70848a3e6af10d66217619ef90f4e72709149
Nick G <nicholasgalante1997@gmail.com>
2022-01-25 18:37:04 -0700

    moving to css modules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Fragment } from 'react';
-import NavBar from '../src/components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Fragment>
-      <NavBar />
       <Component {...pageProps} />
-    </Fragment>
   );
 }
 
